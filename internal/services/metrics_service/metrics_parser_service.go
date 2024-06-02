ff2f622034750db24b1bcf61480f7b197e173350
alex_black <alexblacknn@yandex.ru>
2024-06-02 13:56:47 +0300

    delete commented code in metrics_parser_service

--- internal/services/metrics_service/metrics_parser_service.go
+++ internal/services/metrics_service/metrics_parser_service.go
@@ -39,61 +39,6 @@ func New(
 	}
 }
 
-//func (ms *MetricService) UpdateMetricValue(
-//	ctx context.Context,
-//	urlPath string,
-//) error {
-//	log := ms.log.With(
-//		slog.String("info", "SERVICE LAYER: metrics_service.UpdateMetricValue"),
-//	)
-//	log.Info("starts update metric value")
-//
-//	parts := strings.Split(strings.Trim(urlPath, "/"), "/")
-//	if len(parts) != 4 {
-//		return ErrNotValidUrl
-//	}
-//
-//	var (
-//		metricType  string
-//		metricValue any
-//		err         error
-//		metric      models.Metric
-//	)
-//
-//	switch metricType = parts[1]; metricType {
-//	case "gauge":
-//		metricValue, err = strconv.ParseFloat(parts[3], 64)
-//		if err != nil {
-//			return ErrNotValidMetricValue
-//		}
-//		metric = models.Metric{Type: metricType, Name: parts[2], Value: metricValue}
-//
-//	case "counter":
-//		metricValue, err = strconv.ParseInt(parts[3], 10, 64)
-//		if err != nil {
-//			return ErrNotValidMetricValue
-//		}
-//		metric, err = ms.metricsStorage.GetMetric(ctx, metric.Name)
-//		if err != nil {
-//			return ErrCouldNotUpdateMetric
-//		}
-//		switch value := metric.Value.(type) {
-//		case int64:
-//			metric.Value = value + metricValue.(int64)
-//		default:
-//			metric.Value = metricValue.(int64)
-//		}
-//	default:
-//		return ErrNotValidMetricType
-//	}
-//
-//	err = ms.metricsStorage.UpdateMetric(ctx, metric)
-//	if err != nil {
-//		return ErrCouldNotUpdateMetric
-//	}
-//	return nil
-//}
-
 func (ms *MetricService) UpdateMetricValue(ctx context.Context, urlPath string) error {
 	log := ms.log.With(
 		slog.String("info", "SERVICE LAYER: metrics_service.UpdateMetricValue"),
