28e222a96e9102cabf2eb5f75d5642c039d75d8d
alex_black <alexblacknn@yandex.ru>
2024-06-02 14:19:40 +0300

    add error for storage layer

--- storage/storage_errors.go
+++ storage/storage_errors.go
@@ -3,6 +3,5 @@ package storage
 import "errors"
 
 var (
-	ErrOperationNotFound = errors.New("operation not found")
-	ErrUserNotFound      = errors.New("user not found")
+	ErrMetricNotFound = errors.New("metric not found")
 )
