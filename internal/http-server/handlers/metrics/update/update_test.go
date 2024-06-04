fa93ba17a02755bccbafe8b777108d6a98dc8416
alex <alexblacknn@yandex.ru>
2024-06-04 23:07:37 +0300

    add some tests postitives and negatives for server

--- internal/http-server/handlers/metrics/update/update_test.go
+++ internal/http-server/handlers/metrics/update/update_test.go
@@ -3,11 +3,11 @@ package update
 import (
 	"github.com/AlexBlackNn/metrics/internal/app_server"
 	"github.com/AlexBlackNn/metrics/internal/config"
-	"github.com/AlexBlackNn/metrics/internal/utils"
 	"github.com/stretchr/testify/assert"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -21,13 +21,49 @@ func TestNew(t *testing.T) {
 
 	tests := []struct {
 		name string
+		url  string
 		want Want
-	}{{
-		name: "positive test #1",
-		want: Want{
-			code:        200,
-			contentType: "text/plain; charset=utf-8",
-		}},
+	}{
+		{
+			name: "positive test gauge",
+			url:  "/update/gauge/Lookups/10",
+			want: Want{
+				code:        http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name: "positive test counter",
+			url:  "/update/counter/PoolCount/10",
+			want: Want{
+				code:        http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name: "negative test metric name absent",
+			url:  "/update/counter/10",
+			want: Want{
+				code:        http.StatusNotFound,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name: "negative test metric wrong type value",
+			url:  "/update/counter/PoolCount/test",
+			want: Want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name: "negative test metric wrong metric type",
+			url:  "/update/histogram/PoolCount/10",
+			want: Want{
+				code:        http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
 	}
 
 	cfg := &config.Config{
@@ -37,15 +73,14 @@ func TestNew(t *testing.T) {
 		ReportInterval: 5,
 	}
 
-	// init logger
-	log := utils.SetupLogger(cfg.Env)
-	log.Info("starting application", slog.String("env", cfg.Env))
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	application := app_server.New(log, cfg)
+
 	handlerUnderTest := New(log, application)
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodPost, "/update/gauge/Lookups/10", nil)
+			request := httptest.NewRequest(http.MethodPost, tt.url, nil)
 			// создаём новый Recorder
 			w := httptest.NewRecorder()
 			handlerUnderTest(w, request)
