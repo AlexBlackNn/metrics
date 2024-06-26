db4c79fa1c5525a7ee3939907c07be5664be48d8
alex <alexblacknn@yandex.ru>
2024-06-26 21:37:13 +0300

    agent client is now v2, which uses json format

--- app/agent/app.go
+++ app/agent/app.go
@@ -2,7 +2,7 @@ package agent
 
 import (
 	"context"
-	"github.com/AlexBlackNn/metrics/app/agent/restagentsender"
+	"github.com/AlexBlackNn/metrics/app/agent/restagentsender/v2"
 	"github.com/AlexBlackNn/metrics/internal/config"
 	"log/slog"
 )
@@ -23,7 +23,7 @@ func NewAppMonitor(
 	cfg *config.Config,
 ) *AppMonitor {
 
-	metricsService := restagentsender.New(
+	metricsService := v2.New(
 		log,
 		cfg,
 	)
---
+++ app/agent/restagentsender/v2/sender.go
@@ -0,0 +1,88 @@
+package v2
+
+import (
+	"context"
+	"fmt"
+	"github.com/AlexBlackNn/metrics/internal/config"
+	"github.com/AlexBlackNn/metrics/internal/domain/models"
+	"github.com/AlexBlackNn/metrics/internal/services/agentmetricsservice"
+	"github.com/go-resty/resty/v2"
+	"log/slog"
+	"time"
+)
+
+type Sender struct {
+	log *slog.Logger
+	cfg *config.Config
+	*agentmetricsservice.MonitorService
+}
+
+func New(
+	log *slog.Logger,
+	cfg *config.Config,
+) *Sender {
+	return &Sender{
+		log,
+		cfg,
+		agentmetricsservice.New(log, cfg),
+	}
+}
+
+func (mhs *Sender) Send(ctx context.Context) {
+
+	log := mhs.log.With(
+		slog.String("info", "SERVICE LAYER: metricsHttpService.Transmit"),
+	)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			for _, savedMetric := range mhs.GetMetrics() {
+				go func(savedMetric models.MetricInteraction) {
+					restyClient := resty.New()
+					restyClient.
+						SetRetryCount(3).
+						SetRetryWaitTime(30 * time.Second).
+						SetRetryMaxWaitTime(90 * time.Second)
+
+					var resp *resty.Response
+					var err error
+					url := fmt.Sprintf("http://%s/update/", mhs.cfg.ServerAddr)
+					log.Info("sending data", "url", url)
+					if savedMetric.GetType() == "counter" {
+						body := fmt.Sprintf(`{"id":"%s", "type":"%s", "delta": %d}`,
+							savedMetric.GetName(),
+							savedMetric.GetType(),
+							savedMetric.GetValue(),
+						)
+						fmt.Println("1111111111111111111111", body)
+						resp, err = restyClient.R().
+							SetHeader("Content-Type", "application/json").
+							SetBody(body).
+							Post(url)
+					} else {
+						fmt.Println(savedMetric.GetName(), savedMetric.GetType())
+						body := fmt.Sprintf(`{"id":"%s", "type":"%s", "value": %v}`,
+							savedMetric.GetName(),
+							savedMetric.GetType(),
+							savedMetric.GetValue(),
+						)
+						fmt.Println("222222222222222222222222", body)
+						resp, err = restyClient.R().
+							SetHeader("Content-Type", "application/json").
+							SetBody(body).
+							Post(url)
+					}
+					if err != nil {
+						log.Error("error creating http request")
+					}
+					log.Info("http request finished successfully", "url", url)
+					fmt.Println(resp)
+					fmt.Println(resp.StatusCode())
+				}(savedMetric)
+			}
+			<-time.After(time.Duration(mhs.cfg.ReportInterval) * time.Second)
+		}
+	}
+}
