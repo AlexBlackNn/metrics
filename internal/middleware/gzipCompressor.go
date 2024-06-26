9dcf0ffb74a562b04d15b149eb153dc9e6dbe8bc
alex <alexblacknn@yandex.ru>
2024-06-26 20:50:14 +0300

    refactoring middleware, delete commented code

--- internal/middleware/gzipCompressor.go
+++ internal/middleware/gzipCompressor.go
@@ -2,7 +2,6 @@ package middleware
 
 import (
 	"compress/gzip"
-	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
@@ -10,35 +9,11 @@ import (
 	"sync"
 )
 
-// content type to compress data
-var compressibleContentTypes = []string{
-	"application/json",
-	"text/html",
-}
-
-var GzipSingleton = make(map[string]*GzipWriter)
-var lock = &sync.Mutex{}
-
 type GzipWriter struct {
 	ResWriter       http.ResponseWriter
 	Writer          *gzip.Writer
 	GzipWriterMutex sync.Mutex
 }
-
-//func New(resWriter http.ResponseWriter, level int) (*GzipWriter, error) {
-//	lock.Lock()
-//	defer lock.Unlock()
-//	if gzipWriter, ok := GzipSingleton["gzip"]; ok {
-//		gzipWriter.Writer.Reset(resWriter)
-//		return gzipWriter, nil
-//	}
-//	gzipWr, err := gzip.NewWriterLevel(resWriter, level)
-//	if err != nil {
-//		io.WriteString(resWriter, err.Error())
-//		return nil, err
-//	}
-//	return &GzipWriter{ResWriter: resWriter, Writer: gzipWr}, nil
-//}
 
 func (w *GzipWriter) Header() http.Header {
 	return w.ResWriter.Header()
@@ -79,30 +54,23 @@ func GzipCompressor(log *slog.Logger, compressorLevel int) func(next http.Handle
 		log := log.With(
 			slog.String("component", "middleware/gzip"),
 		)
-		log.Info("gzip middleware enabled")
+		log.Info("gzip compressor enabled")
 		fn := func(w http.ResponseWriter, r *http.Request) {
-
-			fmt.Println("*****", r.Header.Values("Accept-Encoding"))
-			fmt.Println("*****", r.Header.Get("Accept-Encoding"))
-
 			if !strings.Contains(strings.Join(r.Header.Values("Accept-Encoding"), " "), "gzip") {
 				// if gzip is not supported then return uncompressed page
 				next.ServeHTTP(w, r)
 				return
 			}
 			log.Info("gzip is supported")
-			//gzipWr, err := New(w, compressorLevel)
 
-			gzipWr, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+			gzipWr, err := gzip.NewWriterLevel(w, compressorLevel)
 			defer gzipWr.Close()
 			if err != nil {
 				io.WriteString(w, err.Error())
 				return
 			}
 			next.ServeHTTP(&GzipWriter{ResWriter: w, Writer: gzipWr}, r)
-
 		}
-
 		return http.HandlerFunc(fn)
 	}
 }
--- internal/middleware/gzipDecompressor.go
+++ internal/middleware/gzipDecompressor.go
@@ -2,15 +2,12 @@ package middleware
 
 import (
 	"compress/gzip"
-	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"strings"
 )
 
-// compressReader реализует интерфейс io.ReadCloser и позволяет прозрачно для сервера
-// декомпрессировать получаемые от клиента данные
 type compressReader struct {
 	r  io.ReadCloser
 	zr *gzip.Reader
@@ -41,26 +38,22 @@ func (c *compressReader) Close() error {
 
 func GzipDecompressor(log *slog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
-
+		log := log.With(
+			slog.String("component", "middleware/gzip"),
+		)
+		log.Info("gzip decompressor middleware enabled")
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			contentEncoding := r.Header.Get("Content-Encoding")
 			sendsGzip := strings.Contains(contentEncoding, "gzip")
 			if sendsGzip {
-				fmt.Println("000000000000000000000000000000000000000000000000000000000000000000000000000000000000000")
-				// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
 				cr, err := newCompressReader(r.Body)
 				if err != nil {
-					fmt.Println("22222222", err.Error())
 					w.WriteHeader(http.StatusInternalServerError)
 					return
 				}
-				// меняем тело запроса на новое
 				r.Body = cr
-				fmt.Println("33333333", r.Body)
 				defer cr.Close()
 			}
-
-			// передаём управление хендлеру
 			next.ServeHTTP(w, r)
 		}
 
