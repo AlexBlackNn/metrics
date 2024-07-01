d73224625a210a624880dc1d3e9962650cc351f4
alex <alexblacknn@yandex.ru>
2024-07-01 21:54:57 +0300

    refactoring gob state manager

--- pkg/storage/memstorage/gob_state_manager.go
+++ pkg/storage/memstorage/gob_state_manager.go
@@ -14,13 +14,9 @@ import (
 func init() {
 	gob.Register(models.Metric[uint64]{})
 	gob.Register(models.Metric[float64]{})
-	gob.Register(TempMetric{})
 	gob.Register(encodeMetricUint64)
 	gob.Register(encodeMetricFloat64)
-	//gob.Register(decodeMetricUint64)
-	//gob.Register(decodeMetricFloat64)
-	gob.Register(encodeMetricGetter)
-	gob.Register(decodeMetricGetter)
+
 }
 
 // dataBaseJSONStateManager saves and restores database state
@@ -42,25 +38,6 @@ func encodeMetricUint64(enc *gob.Encoder, m models.Metric[uint64]) error {
 	})
 }
 
-// Custom gob decoder for models.Metric[uint64]
-func decodeMetricUint64(dec *gob.Decoder, m *models.Metric[uint64]) error {
-	var data struct {
-		Name  string
-		Type  string
-		Value uint64 // Match the encoding type here
-	}
-	if err := dec.Decode(&data); err != nil {
-		return err
-	}
-
-	*m = models.Metric[uint64]{
-		Name:  data.Name,
-		Type:  data.Type,
-		Value: data.Value,
-	}
-	return nil
-}
-
 // Custom gob encoder for models.Metric[uint64]
 func encodeMetricFloat64(enc *gob.Encoder, m models.Metric[float64]) error {
 	return enc.Encode(struct {
@@ -74,60 +51,6 @@ func encodeMetricFloat64(enc *gob.Encoder, m models.Metric[float64]) error {
 	})
 }
 
-// Custom gob decoder for models.Metric[float64]
-func decodeMetricFloat64(dec *gob.Decoder, m *models.Metric[float64]) error {
-	var data struct {
-		Name  string
-		Type  string
-		Value float64 // Match the encoding type here
-	}
-	if err := dec.Decode(&data); err != nil {
-		return err
-	}
-	*m = models.Metric[float64]{
-		Name:  data.Name,
-		Type:  data.Type,
-		Value: data.Value,
-	}
-	return nil
-}
-
-// Custom encoder for models.MetricGetter
-func encodeMetricGetter(enc *gob.Encoder, m models.MetricGetter) error {
-	return enc.Encode(TempMetric{
-		Type:  m.GetType(),
-		Name:  m.GetName(),
-		Value: m.GetValue(), // You might need to adjust how GetValue() is handled
-	})
-}
-
-// Custom decoder for models.MetricGetter
-func decodeMetricGetter(dec *gob.Decoder, m *models.MetricGetter) error {
-	var temp TempMetric
-	if err := dec.Decode(&temp); err != nil {
-		return err
-	}
-
-	// Convert TempMetric to the appropriate models.Metric type
-	switch temp.Type {
-	case "counter":
-		// Convert TempMetric.Value to uint64 if necessary
-		// *m = models.Metric[uint64]{
-		//  Name:  temp.Name,
-		//  Type:  temp.Type,
-		//  Value: temp.Value.(uint64),
-		// }
-	default:
-		// Convert TempMetric.Value to float64 if necessary
-		// *m = models.Metric[float64]{
-		//  Name:  temp.Name,
-		//  Type:  temp.Type,
-		//  Value: temp.Value.(float64),
-		// }
-	}
-
-	return nil
-}
 func (gm *dataBaseGOBStateManager) saveMetrics() error {
 
 	file, err := os.OpenFile(
