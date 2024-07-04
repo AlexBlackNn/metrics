de942b29f1e98c1db0882922e8f73f19315370c2
alex <alexblacknn@yandex.ru>
2024-07-04 11:40:20 +0300

    add rate limiter to server as middleware to prevent server overfloading on server side

--- cmd/server/router/chi_router.go
+++ cmd/server/router/chi_router.go
@@ -7,13 +7,21 @@ import (
 	customMiddleware "github.com/AlexBlackNn/metrics/internal/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httprate"
 	"log/slog"
+	"time"
 )
 
 func NewChiRouter(log *slog.Logger, metricHandlerV1 v1.MetricHandlers, metricHandlerV2 v2.MetricHandlers) *chi.Mux {
 
 	router := chi.NewRouter()
 	router.Use(middleware.RequestID)
+	//	Rate limit by IP and URL path (aka endpoint)
+	router.Use(httprate.Limit(
+		100,           // requests
+		1*time.Second, // per duration
+		httprate.WithKeyFuncs(httprate.KeyByIP, httprate.KeyByEndpoint),
+	))
 	router.Use(customMiddleware.Logger(log))
 	router.Use(customMiddleware.GzipDecompressor(log))
 	router.Use(customMiddleware.GzipCompressor(log, gzip.BestCompression))
