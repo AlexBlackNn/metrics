3ed427cb1d7a9409ec4e16fec1bdec98edb04d70
alex <alexblacknn@yandex.ru>
2024-07-13 17:46:11 +0300

    add v3 sender code to send data to server

--- app/agent/restagentsender/v3/sender.go
+++ app/agent/restagentsender/v3/sender.go
@@ -1,11 +1,10 @@
-package v2
+package v3
 
 import (
 	"context"
 	"fmt"
 	"github.com/AlexBlackNn/metrics/internal/config/configagent"
 	"github.com/AlexBlackNn/metrics/internal/config/configserver"
-	"github.com/AlexBlackNn/metrics/internal/domain/models"
 	"github.com/AlexBlackNn/metrics/internal/services/agentmetricsservice"
 	"github.com/go-resty/resty/v2"
 	"golang.org/x/time/rate"
@@ -42,47 +41,48 @@ func (mhs *Sender) Send(ctx context.Context) {
 		case <-ctx.Done():
 			return
 		default:
+
+			body := "["
 			for _, savedMetric := range mhs.GetMetrics() {
+				if savedMetric.GetType() == configserver.MetricTypeCounter {
+					body += fmt.Sprintf(`{"id":"%s", "type":"%s", "delta": %d}`,
+						savedMetric.GetName(),
+						savedMetric.GetType(),
+						savedMetric.GetValue(),
+					)
+				} else {
+					body += fmt.Sprintf(`{"id":"%s", "type":"%s", "value": %v}`,
+						savedMetric.GetName(),
+						savedMetric.GetType(),
+						savedMetric.GetValue(),
+					)
+				}
+				body += "]"
 				err := rateLimiter.Wait(ctx)
 				if err != nil {
 					log.Error(err.Error())
 				}
-				go func(savedMetric models.MetricInteraction) {
-					restyClient := resty.New()
-					restyClient.
-						SetRetryCount(mhs.cfg.AgentRetryCount).
-						SetRetryWaitTime(mhs.cfg.AgentRetryWaitTime).
-						SetRetryMaxWaitTime(mhs.cfg.AgentRetryMaxWaitTime)
 
-					var body string
-					if savedMetric.GetType() == configserver.MetricTypeCounter {
-						body = fmt.Sprintf(`{"id":"%s", "type":"%s", "delta": %d}`,
-							savedMetric.GetName(),
-							savedMetric.GetType(),
-							savedMetric.GetValue(),
-						)
-					} else {
-						body = fmt.Sprintf(`{"id":"%s", "type":"%s", "value": %v}`,
-							savedMetric.GetName(),
-							savedMetric.GetType(),
-							savedMetric.GetValue(),
-						)
-					}
-					url := fmt.Sprintf("http://%s/update/", mhs.cfg.ServerAddr)
-					log.Info("sending data", "url", url)
-					resp, err := restyClient.R().
-						SetHeader("Content-Type", "application/json").
-						SetBody(body).
-						Post(url)
-					if err != nil {
-						log.Error("error creating http request")
-					}
-					log.Info("http request finished successfully",
-						"url", url,
-						"statusCode", resp.StatusCode(),
-						"body", string(resp.Body()),
-					)
-				}(savedMetric)
+				restyClient := resty.New()
+				restyClient.
+					SetRetryCount(mhs.cfg.AgentRetryCount).
+					SetRetryWaitTime(mhs.cfg.AgentRetryWaitTime).
+					SetRetryMaxWaitTime(mhs.cfg.AgentRetryMaxWaitTime)
+
+				url := fmt.Sprintf("http://%s/updates/", mhs.cfg.ServerAddr)
+				log.Info("sending data", "url", url)
+				resp, err := restyClient.R().
+					SetHeader("Content-Type", "application/json").
+					SetBody(body).
+					Post(url)
+				if err != nil {
+					log.Error("error creating http request")
+				}
+				log.Info("http request finished successfully",
+					"url", url,
+					"statusCode", resp.StatusCode(),
+					"body", string(resp.Body()),
+				)
 			}
 			<-time.After(reportInterval)
 		}
