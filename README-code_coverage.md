6e5a46ae230f07ddea45256821cea9f4cba7fad3
alex <alexblacknn@yandex.ru>
2024-10-05 11:11:00 +0300

    add tests for temp metric in storages

--- README-code_coverage.md
+++ README-code_coverage.md
@@ -6,3 +6,9 @@ go test -v ./...
 go test -v -coverpkg=./... -coverprofile=profile.cov ./...
 go tool cover -func profile.cov
 ```
+
+```bash
+go test -v ./... -coverprofile profile.out
+go tool cover -func profile.out
+go tool cover -o coverage.html -html=profile.out; sed -i 's/black/whitesmoke/g' coverage.html; sensible-browser coverage.html
+```
---
+++ pkg/storage/memstorage/temp_metric_test.go
@@ -0,0 +1,56 @@
+package memstorage
+
+import (
+	"testing"
+)
+
+func TestMetricHash(t *testing.T) {
+	var tests = []struct {
+		metricType        string
+		metricName        string
+		metricValue       any
+		metricStringValue string
+	}{
+		{"counter",
+			"test_counter",
+			int64(10),
+			"10",
+		},
+		{"counter",
+			"test_counter",
+			float64(10),
+			"10",
+		},
+		{"gauge",
+			"test_gauge",
+			0.21,
+			"0.21",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.metricName, func(t *testing.T) {
+			tempMetric := TempMetric{
+				test.metricType,
+				test.metricName,
+				test.metricValue,
+			}
+			metricTypeGot := tempMetric.GetType()
+			if metricTypeGot != test.metricType {
+				t.Errorf("got %s, want %s", metricTypeGot, test.metricType)
+			}
+			metricNameGot := tempMetric.GetName()
+			if metricNameGot != test.metricName {
+				t.Errorf("got %s, want %s", metricNameGot, test.metricName)
+			}
+			metricValueGot := tempMetric.GetValue()
+			if metricValueGot != test.metricValue {
+				t.Errorf("got %v, want %v", metricValueGot, test.metricType)
+			}
+			metricStringValueGot := tempMetric.GetStringValue()
+			if metricStringValueGot != test.metricStringValue {
+				t.Errorf("got %v, want %v", metricStringValueGot, test.metricStringValue)
+			}
+		})
+	}
+}
