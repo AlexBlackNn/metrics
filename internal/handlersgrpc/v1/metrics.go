56a2bd9311613763ba45faf60735c95c8d0e0d8a
alex <alexblacknn@yandex.ru>
2024-11-10 11:08:12 +0300

    add update handler

--- internal/handlersgrpc/v1/metrics.go
+++ internal/handlersgrpc/v1/metrics.go
@@ -2,7 +2,12 @@ package v1
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"time"
 
+	"github.com/AlexBlackNn/metrics/internal/config/configserver"
+	"github.com/AlexBlackNn/metrics/internal/domain/models"
 	"github.com/AlexBlackNn/metrics/internal/services/metricsservice"
 	metricsgrpc_v1 "github.com/AlexBlackNn/metrics/metricsgrpc"
 	"google.golang.org/grpc"
@@ -43,6 +48,43 @@ func (s *serverAPI) UpdateMetric(ctx context.Context, metricgrpc *metricsgrpc_v1
 	*metricsgrpc_v1.Response,
 	error,
 ) {
+	var metricRecieved models.MetricInteraction
+	var err error
+
+	if metricgrpc.GetType() == configserver.MetricTypeCounter {
+		metricRecieved, err = models.New(
+			metricgrpc.GetType(),
+			metricgrpc.GetId(),
+			fmt.Sprintf("%d", metricgrpc.GetDelta()),
+		)
+	} else {
+		metricRecieved, err = models.New(
+			metricgrpc.GetType(),
+			metricgrpc.GetId(),
+			fmt.Sprintf("%g", metricgrpc.GetValue()),
+		)
+	}
+	if err != nil {
+		return &metricsgrpc_v1.Response{
+			Error: "data validation failed",
+		}, errors.New("data validation failed")
+
+	}
+
+	ctx, cancel := context.WithTimeoutCause(ctx, 300*time.Millisecond, errors.New("updateMetric timeout"))
+	defer cancel()
+
+	err = s.metric.UpdateMetricValue(ctx, metricRecieved)
+	if err != nil {
+		if errors.Is(err, metricsservice.ErrNotValidURL) {
+			return &metricsgrpc_v1.Response{
+				Error: "metric not found",
+			}, errors.New("not found")
+		}
+		return &metricsgrpc_v1.Response{
+			Error: "internal server error",
+		}, errors.New("internal server error")
+	}
 
 	return &metricsgrpc_v1.Response{
 		Status: "ok",
